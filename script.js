ede973e4b2b91d4d1ce2ab1a66c92d27ba1df915
galiakhmetovf@gmail.com <galiakhmetov.f@gmail.com>
2024-11-28 21:04:47 +0500

    finished script.js and index.html second commit

--- script.js
+++ script.js
@@ -1,32 +1,79 @@
 const arrLet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const arrNum = []; 
+const sLet =   ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]; 
 let input = document.querySelector('input');
 let textarea = document.querySelector("textarea");
 let exit = document.querySelector('.exit');
-let c=0;
-let d;
+
+function resultString(inputnum, string, array) {
+  let str = "";
+  let strIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+     if (array[i] === string[strIndex] && strIndex < string.length){  
+        let r = i+inputnum;
+        if(r>= array.length){
+          r = r-array.length;  
+          str+= array[r];
+          strIndex++;
+          i=-1;
+        } else {
+          str+= array[r];
+          strIndex++;
+          i=-1;
+        }  
+      } else if(!(array[i]===string[strIndex]) && strIndex < string.length && i===25){
+         
+          str+= string[strIndex];
+          strIndex++;
+          i=-1;
+     }      
+  }
+  return str;
+}/*function resultString*/
+
 function shifr(){ 
-    let b;
+let w = Number(input.value);
+  const small = resultString(w, textarea.value, sLet);
+  const big   = resultString(w, small, arrLet);
+  
+  
+
+  exit.innerHTML = big;  
     
-    for(let l=0; l<arrLet.length; l++){
-      if(textarea.value[c] === arrLet[l]){
-       c++; 
-       b=Number(input.value);
-       
-       exit.innerHTML+=arrLet[l+b];
-      }  
-      if(textarea.value.length<exit.innerHTML.length){
-        exit.innerHTML=exit.innerHTML.slice(0, -1);
-        c--;
-      } 
-    }  
-        
-    
-      
 }
 
 textarea.oninput = shifr;
-/*
-exit.innerHTML="farid";
-exit.innerHTML=exit.innerHTML.slice(0, -1);
-console.log(exit.innerHTML) */
+input.oninput = shifr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
